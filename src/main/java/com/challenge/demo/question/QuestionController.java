5b3a68b7bdd4a2d4bb8bcc3cc5ec24b2a2fc15be
Willian F. Batista <wfuertes@gmail.com>
2021-02-25 18:25:53 -0600

    Remove unnecessary @ResponseBody, small adjustments on *Controllers

--- src/main/java/com/challenge/demo/question/QuestionController.java
+++ src/main/java/com/challenge/demo/question/QuestionController.java
@@ -12,11 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/questions")
@@ -33,60 +31,49 @@ public class QuestionController {
     }
 
     @PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<QuestionDTO> createQuestion(@RequestBody QuestionDTO incomingQuestion) {
-		return siteRepository
-				.findById(incomingQuestion.getSiteId())
-				.map(site -> {
-					final Question newQ = QuestionDTO.createQuestion(incomingQuestion, site);
-					return new ResponseEntity<>(QuestionDTO.build(questionRepository.save(newQ)), HttpStatus.CREATED);
-				})
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    public ResponseEntity<QuestionDTO> createQuestion(@RequestBody QuestionDTO incomingQuestion) {
+        return siteRepository.findById(incomingQuestion.getSiteId())
+                             .map(site -> {
+                                 final Question newQ = QuestionDTO.createQuestion(incomingQuestion, site);
+                                 return new ResponseEntity<>(QuestionDTO.build(questionRepository.save(newQ)), HttpStatus.CREATED);
+                             })
+                             .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 	@GetMapping
-	public ResponseEntity<List<QuestionDTO>> getSites() {
-		return Optional
-				.ofNullable(questionRepository.findAll())
-				.map(questions -> ResponseEntity.ok(QuestionDTO.build(questions)))
-				.orElseGet(() -> ResponseEntity.notFound().build());
+	public ResponseEntity<List<QuestionDTO>> getQuestions() {
+		return ResponseEntity.ok(QuestionDTO.build(questionRepository.findAll()));
 	}
 
-	@PutMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<QuestionDTO> updateQuestion(@RequestBody Question incomingQuestion, @PathVariable(value = "id") Long questionId) {
+    @PutMapping("/{id}")
+    public ResponseEntity<QuestionDTO> updateQuestion(@RequestBody Question incomingQuestion, @PathVariable(value = "id") Long questionId) {
+        return questionRepository.findById(questionId)
+                                 .map(question -> {
+                                     question.setQuestion(incomingQuestion.getQuestion());
+                                     question.setSite(incomingQuestion.getSite());
+                                     return ResponseEntity.ok(QuestionDTO.build(questionRepository.save(question)));
+                                 })
+                                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-		return questionRepository
-				.findById(questionId)
-				.map(question -> {
-					question.setQuestion(incomingQuestion.getQuestion());
-					question.setSite(incomingQuestion.getSite());
-					return new ResponseEntity<>(QuestionDTO.build(questionRepository.save(question)), HttpStatus.OK);
-				})
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<QuestionDTO> deleteQuestion(@PathVariable(value = "id") Long questionId) {
+        return questionRepository.findById(questionId)
+                                 .map(question -> {
+                                     questionRepository.delete(question);
+                                     return ResponseEntity.ok(QuestionDTO.build(question));
+                                 })
+                                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<QuestionDTO> deleteQuestion(@PathVariable(value = "id") Long questionId) {
-		return questionRepository
-				.findById(questionId)
-				.map(question -> {
-					questionRepository.delete(question);
-					return ResponseEntity.ok(QuestionDTO.build(question));
-				})
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<QuestionDTO> getQuestionById(@PathVariable(value = "id") Long questionId) {
-		return questionRepository
-				.findById(questionId)
-				.map(question -> ResponseEntity.ok(QuestionDTO.build(question)))
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<QuestionDTO> getQuestionById(@PathVariable(value = "id") Long questionId) {
+        return questionRepository.findById(questionId)
+                                 .map(question -> ResponseEntity.ok(QuestionDTO.build(question)))
+                                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 	@PostMapping("/{id}/answers")
-	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<QuestionAnswerDTO> createQuestionAnswers(@PathVariable(value = "id") Long questionId,
                                                                    @RequestBody QuestionAnswerDTO newQADto) {
 		return questionRepository
@@ -98,12 +85,10 @@ public class QuestionController {
 				.orElseGet(() -> ResponseEntity.notFound().build());
 	}
 
-	@GetMapping("/{id}/answers")
-	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<List<QuestionAnswerDTO>> getQuestionAnswers(@PathVariable(value = "id") Long questionId) {
-		return questionRepository
-				.findById(questionId)
-				.map(question -> ResponseEntity.ok(QuestionAnswerDTO.build(question.getAnswers())))
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    @GetMapping("/{id}/answers")
+    public ResponseEntity<List<QuestionAnswerDTO>> getQuestionAnswers(@PathVariable(value = "id") Long questionId) {
+        return questionRepository.findById(questionId)
+                                 .map(question -> ResponseEntity.ok(QuestionAnswerDTO.build(question.getAnswers())))
+                                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/challenge/demo/site/SiteController.java
+++ src/main/java/com/challenge/demo/site/SiteController.java
@@ -9,12 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -28,50 +25,42 @@ public class SiteController {
     }
 
     @PostMapping
-	@ResponseBody
-	@ResponseStatus(HttpStatus.CREATED)
-	public Site createSite(@RequestBody Site createSite) {
+	public ResponseEntity<Site> createSite(@RequestBody Site createSite) {
 		createSite.setSiteUUID(UUID.randomUUID());
 
-		return siteRepository.save(createSite);
+		return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(siteRepository.save(createSite));
 	}
 
-	@GetMapping()
+	@GetMapping
 	public ResponseEntity<List<Site>> getSites() {
-		return Optional
-				.ofNullable(siteRepository.findAll())
-				.map(sites -> ResponseEntity.ok(sites))
-				.orElseGet(() -> ResponseEntity.notFound().build());
+        return ResponseEntity.ok(siteRepository.findAll());
 	}
 
-	@PutMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<Site> updateSite(@RequestBody Site updatedSite, @PathVariable(value = "id") Long siteId) {
-		return siteRepository
-				.findById(siteId)
-				.map(site -> {
-					site.setUrl(updatedSite.getUrl());
-					return new ResponseEntity<>(siteRepository.save(site), HttpStatus.OK);
-				})
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    @PutMapping("/{id}")
+    public ResponseEntity<Site> updateSite(@RequestBody Site updatedSite, @PathVariable(value = "id") Long siteId) {
+        return siteRepository.findById(siteId)
+                             .map(site -> {
+                                 site.setUrl(updatedSite.getUrl());
+                                 return new ResponseEntity<>(siteRepository.save(site), HttpStatus.OK);
+                             })
+                             .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Site> deleteSite(@PathVariable(value = "id") Long siteId) {
-		return siteRepository
-				.findById(siteId)
-				.map(site -> {
-					siteRepository.delete(site);
-					return ResponseEntity.ok(site);
-				})
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Site> deleteSite(@PathVariable(value = "id") Long siteId) {
+        return siteRepository.findById(siteId)
+                             .map(site -> {
+                                 siteRepository.delete(site);
+                                 return ResponseEntity.ok(site);
+                             })
+                             .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Site> getSiteById(@PathVariable(value = "id") Long siteId) {
-		return siteRepository
-				.findById(siteId)
-				.map(site -> ResponseEntity.ok(site))
-				.orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<Site> getSiteById(@PathVariable(value = "id") Long siteId) {
+        return siteRepository.findById(siteId)
+                             .map(ResponseEntity::ok)
+                             .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
